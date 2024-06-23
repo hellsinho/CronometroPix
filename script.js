b1b15c6fd66c1009e043368b1ebce9594a0af955
Helson Gonçalves <helson.santos25@gmail.com>
2024-06-23 13:44:16 -0300

    cronometro pix

---
+++ script.js
@@ -0,0 +1,24 @@
+function startTimer(duration, display) {
+    let timer = duration, hours, minutes, seconds;
+    setInterval(function () {
+        hours = parseInt(timer / 3600, 10);
+        minutes = parseInt((timer % 3600) / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = hours + ":" + minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    let countdownTime = 3600 * 240; // 3 hours in seconds
+    let display = document.querySelector('#timer');
+    startTimer(countdownTime, display);
+};
